0362740d16f1889c29e1ca733bf4c7a5ce0df8ae
Lorenzo <tunedconsulting@gmail.com>
2015-08-29 22:27:05 +0200

    corrected variables in config.py

--- config/config.py
+++ config/config.py
@@ -21,21 +21,19 @@ _TEMP_SECRET = "***********"
 def set_env_variables():
     if not 'SERVER_SOFTWARE' in os.environ or os.environ['SERVER_SOFTWARE'].startswith('Development'):
         _SERVICE = "http://localhost:8080"
-        _REST_SERVICE = "http://localhost:8080/database/cots/"
-        _COMPONENTS_URL = "http://localhost:8080/database/cots/store"
-        _HYDRA_VOCAB = "http://localhost:8080/hydra/vocab"
         _DEBUG = True
     else:
-        _SERVICE = "http://rdfendpoints.appspot.com"
-        _REST_SERVICE = "http://rdfendpoints.appspot.com/database/cots/"
-        _COMPONENTS_URL = "http://rdfendpoints.appspot.com/database/cots/store"
-        _HYDRA_VOCAB = "http://rdfendpoints.appspot.com/hydra/vocab"
+        _SERVICE = "http://hypermedia.projectchronos.eu"
         _DEBUG = True
+
+    _REST_SERVICE = _SERVICE + "/database/cots/"
+    _COMPONENTS_URL = _SERVICE + "/database/cots/store"
+    _HYDRA_VOCAB = _SERVICE + "/hydra/vocab"
 
     return _SERVICE, _REST_SERVICE, _COMPONENTS_URL, _HYDRA_VOCAB, _DEBUG
 
 _SERVICE, _REST_SERVICE, _COMPONENTS_URL, _HYDRA_VOCAB, _DEBUG = set_env_variables()
 
 _CRAWLING_POST = {'local': 'http://localhost:8080/database/crawling/store',
-                  'remote': 'http://rdfendpoints.appspot.com/database/crawling/store'}
+                  'remote': 'http://hypermedia.projectchronos.eu/database/crawling/store'}
 _PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates')
