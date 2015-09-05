b0c40e0c6ee7d26949ca697d59bc0168aaacf3a7
Lorenzo <tunedconsulting@gmail.com>
2015-09-05 15:02:23 +0200

    HTTP endpoints tests completed

--- unittesting/tests_HTTP.py
+++ unittesting/tests_HTTP.py
@@ -17,15 +17,20 @@ def test_integrity(res):
 
 
 class HTTPendpointsTest(unittest.TestCase):
+    def __init__(self, env=None):
+        super(HTTPendpointsTest, self).__init__()
+        if env is not None:
+            self.test_env = env
+        else:
+            self.test_env = 'offline'
 
-    test_env = 'offline'
-
-    def test_sparql(self, env=test_env):
+    def test_sparql(self):
         """
     Test content of the /database/cots/ endpoint and if contained urls are reachable
     :param env: 'offline' for localhost, 'online' for remote
     """
         print "Running test_sparql"
+        env = self.test_env
         base_url = _ENV[env]['_SERVICE'] + "/sparql"
         #
         queries = ["SELECT * WHERE { ?satellites <http://ontology.projectchronos.eu/astronomy/orbitsPlanet> ?planets. }",
@@ -37,13 +42,14 @@ class HTTPendpointsTest(unittest.TestCase):
             print i, r
             test_integrity(r)
 
-    def test_json(self, env=test_env):
+    def test_json(self):
         """
     Test content of the /database/cots/ endpoint and if contained urls are reachable
     :param env: 'offline' for localhost, 'online' for remote
     """
         print "Running test_json"
         import urllib
+        env = self.test_env
 
         base_url = _ENV[env]['_SERVICE'] + "/database/cots/"
         response = get_curling(base_url)
@@ -58,23 +64,25 @@ class HTTPendpointsTest(unittest.TestCase):
                        else v in names
                        for k, v in r.items())
 
-    def test_jsonld(self, env=test_env):
+    def test_jsonld(self):
         """
     Test the hypermedia endpoints: /hydra/spacecraft/
     """
         print "Running test_jsonld"
         import urllib
+        env = self.test_env
 
         base_url = _ENV[env]['_SERVICE'] + "/hydra/spacecraft/"
         response = urllib.urlopen(base_url).read()
         test_integrity(response)
 
-    def test_articles(self, env=test_env):
+    def test_articles(self):
         """
     Test the NL API: /visualize/articles/?api=true
     """
         print "Running test_articles"
         import urllib
+        env = self.test_env
 
         base_url = _ENV[env]['_SERVICE'] + "/visualize/articles/"
 
@@ -90,8 +98,12 @@ class HTTPendpointsTest(unittest.TestCase):
             bookmark = response['next']
             print i, bookmark
 
-
-
+    def runTest(self):
+        run = HTTPendpointsTest(env='offline')
+        run.test_sparql()
+        run.test_json()
+        run.test_jsonld()
+        run.test_articles()
 
 
 
