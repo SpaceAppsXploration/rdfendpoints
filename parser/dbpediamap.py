b6b9bd4d42815647e9fad478496398ec2f73852d
Lorenzo <tunedconsulting@gmail.com>
2015-09-07 13:40:43 +0200

    dbbedia properties map to natural language

---
+++ parser/dbpediamap.py
@@ -0,0 +1,35 @@
+__author__ = 'Lorenzo'
+
+
+planet_mapper = {
+    '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>': 'planet type',  # link to yago category, can be explored more
+    '<http://live.dbpedia.org/ontology/wikiPageExternalLink>': 'external link',  # many
+    '<http://live.dbpedia.org/property/inclination>': 'inclination',  # quantity and text
+    '<http://www.w3.org/2000/01/rdf-schema#seeAlso>': 'see also',  # many
+    '<http://live.dbpedia.org/property/albedo>': 'albedo',  # quantity
+    '<http://xmlns.com/foaf/0.1/depiction>': 'depiction',  # svg shape
+    '<http://live.dbpedia.org/property/rotVelocity>': 'rotation velocity',  # quantity
+    '<http://live.dbpedia.org/property/period>': 'period',  # quantity
+    '<http://live.dbpedia.org/property/meanTemp>': 'average temperature',  # quantity
+    '<http://live.dbpedia.org/ontology/abstract>': 'abstract',  # text
+    '<http://live.dbpedia.org/property/meanAnomaly>': 'average anomaly',   # quantity
+    '<http://live.dbpedia.org/property/siderealDay>': 'sideral day',  # quantity
+    '<http://live.dbpedia.org/property/scaleHeight>': 'atmospheric scale height',  # quantity
+    '<http://live.dbpedia.org/property/mass>': 'mass',  # quantity
+    '<http://live.dbpedia.org/property/escapeVelocity>': 'escape velocity (Km/s)',  # quantity
+    '<http://live.dbpedia.org/property/atmosphere>': 'has atmosphere',  # yes/no
+    '<http://live.dbpedia.org/property/ascNode>': 'asc node',  # quantity
+    '<http://live.dbpedia.org/property/surfaceArea>': 'surface area',  # quantity
+    '<http://live.dbpedia.org/property/equatorialRadius>': 'equatorial radius',  # quantity
+    '<http://live.dbpedia.org/property/polarRadius>': 'polar radius',  # quantity
+    '<http://live.dbpedia.org/ontology/escapeVelocity>': 'escape velocity (double)',  # quantity
+    '<http://live.dbpedia.org/property/atmosphereComposition>': 'atmosphere chemistry',  # text
+    '<http://live.dbpedia.org/property/surfacePressure>': 'surface pressure',
+    '<http://live.dbpedia.org/property/volume> ': 'volume',
+    '<http://live.dbpedia.org/property/angularSize>': 'angular size',
+    '<http://live.dbpedia.org/property/avgSpeed>': 'average speed (Km/s)',
+    '<http://live.dbpedia.org/property/declination>': 'declination',
+    '<http://live.dbpedia.org/property/surfaceGrav>': 'surface gravity (grams)',
+    '<http://live.dbpedia.org/property/satellites>': 'number of satellites'
+}
+
