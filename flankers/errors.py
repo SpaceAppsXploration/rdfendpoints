ae86d51923ef13ff6b22c4b44a701bfafff63cc4
Lorenzo <tunedconsulting@gmail.com>
2015-10-06 00:47:55 +0200

    Add error handling for third parties API's errors

--- flankers/errors.py
+++ flankers/errors.py
@@ -1,9 +1,18 @@
-__author__ = 'lorenzo'
-
 from config.config import _REST_SERVICE, _SERVICE
+
+__author__ = 'lorenzo'
 
 
 def format_message(exception, root=_REST_SERVICE):
     if root == 'hydra':
         root = _SERVICE + '/hydra/'
     return str({"error": 1, "exception": exception, "back": root})
+
+
+class RESTerror(Exception):
+    """
+    Any error message coming from a REST API
+    """
+
+
+
